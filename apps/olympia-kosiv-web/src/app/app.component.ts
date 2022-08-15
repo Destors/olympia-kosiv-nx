2dc8530e6b5f03b04b0ad01d67b33aed52889bdb
Destors <halen54001@gmail.com>
2022-08-15 23:25:31 +0300

    initial commit

---
+++ apps/olympia-kosiv-web/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'olympia-kosiv-nx-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'olympia-kosiv-web';
+}
