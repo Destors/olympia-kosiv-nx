1f7d4591a4dda1fa526b0c302abbf1b3d4ace113
destors <halen54001@gmail.com>
2022-10-21 21:13:53 +0300

    nx g container lib

---
+++ libs/ui/container/src/lib/ui-container.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContainerComponent } from './container/container.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [ContainerComponent],
+})
+export class UiContainerModule {}
