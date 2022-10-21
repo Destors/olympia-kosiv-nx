0a8756c241d0f4a0a6f54978e08e832a73fd3055
destors <halen54001@gmail.com>
2022-10-21 21:50:11 +0300

    add basic layout lib

---
+++ libs/ui/layout/src/lib/layout/layout.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'olympia-kosiv-nx-layout',
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.scss'],
+})
+export class LayoutComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
