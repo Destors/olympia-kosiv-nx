b22161df967259798c4c22757548dada62ab35bc
destors <halen54001@gmail.com>
2022-10-22 16:41:18 +0300

    add basic container to layout

--- libs/ui/layout/src/lib/ui-layout.module.ts
+++ libs/ui/layout/src/lib/ui-layout.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { UiContainerModule } from '@olympia-kosiv-nx/ui/container';
 
 import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, UiContainerModule],
   declarations: [LayoutComponent],
   exports: [LayoutComponent],
 })
