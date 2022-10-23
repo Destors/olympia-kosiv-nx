f497911b7fdb090ea80bae27edacf234bdd7df18
destors <halen54001@gmail.com>
2022-10-23 21:56:01 +0300

    fix localize bug and start test carousel

--- libs/landing/home-page/src/lib/home-page/home-page.component.ts
+++ libs/landing/home-page/src/lib/home-page/home-page.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'olympia-kosiv-nx-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
 })
-export class HomePageComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class HomePageComponent {
+  images: string[];
+  constructor() {
+    this.images = [
+      'https://www.hussle.com/blog/wp-content/uploads/2020/10/5-exercises-full-body-1080x675.jpg',
+      'https://www.hussle.com/blog/wp-content/uploads/2020/10/5-exercises-full-body-1080x675.jpg',
+    ];
+  }
 }
