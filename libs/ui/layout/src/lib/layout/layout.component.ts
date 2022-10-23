81265ac86b3234691cab1ddcd4b8737ce9ecd69c
destors <halen54001@gmail.com>
2022-10-23 20:24:16 +0300

    fix layout switch

--- libs/ui/layout/src/lib/layout/layout.component.ts
+++ libs/ui/layout/src/lib/layout/layout.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Breakpoints } from '@angular/cdk/layout';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { LayoutService } from '@olympia-kosiv-nx/core/layout/service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'olympia-kosiv-nx-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LayoutComponent {
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  constructor() {}
+export class LayoutComponent implements OnInit {
+  readonly breakpoints = Breakpoints;
+
+  layoutType$!: Observable<string>;
+
+  constructor(private readonly layoutService: LayoutService) {}
+
+  ngOnInit(): void {
+    this.layoutType$ = this.layoutService.layoutType$;
+  }
 }
