43ce50c8f6fa1e6adb4872155f1227624d5ce609
destors <halen54001@gmail.com>
2022-10-24 21:43:57 +0300

    destors: add basic firebase get for price page

--- libs/landing/price-page/src/lib/price-page/price-page.component.ts
+++ libs/landing/price-page/src/lib/price-page/price-page.component.ts
@@ -1,12 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
+
+export interface PriceList {
+  landing: string;
+  pricePage: string;
+}
 
 @Component({
   selector: 'olympia-kosiv-nx-price-page',
   templateUrl: './price-page.component.html',
   styleUrls: ['./price-page.component.scss'],
 })
-export class PricePageComponent implements OnInit {
-  constructor() {}
+export class PricePageComponent {
+  items: Observable<any[]>;
 
-  ngOnInit(): void {}
+  constructor(firestore: AngularFirestore) {
+    this.items = firestore.collection('landing').valueChanges();
+  }
 }
