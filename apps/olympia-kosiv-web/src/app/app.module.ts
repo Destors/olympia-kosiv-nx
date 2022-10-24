83b510ea85c8db6ef3b4bd460d932e6f3568d668
destors <halen54001@gmail.com>
2022-10-24 16:32:46 +0300

    destors: Add Firestore

--- apps/olympia-kosiv-web/src/app/app.module.ts
+++ apps/olympia-kosiv-web/src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 
+import { environment } from '../../src/environments/environment';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
@@ -15,6 +18,8 @@ import { AppRoutingModule } from './app-routing.module';
     AppRoutingModule,
     RouterModule,
     CommonModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
