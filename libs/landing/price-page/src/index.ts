8b29a2f32b456fd83267c3552032a019751ae76a
destors <halen54001@gmail.com>
2022-10-24 16:49:02 +0300

    destors: nx rm bug buildable price page lib

--- libs/landing/price-page/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './lib/landing-price-page.module';
-
-export * from './lib/lib.routes';
--- libs/landing/price-page/src/lib/lib.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Route } from '@angular/router';
-
-import { PricePageComponent } from './price-page/price-page.component';
-
-export const landingPricePageRoutes: Route[] = [
-  { path: '', pathMatch: 'full', component: PricePageComponent },
-];
