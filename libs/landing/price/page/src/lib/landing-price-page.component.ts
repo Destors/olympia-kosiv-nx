adb93e410b38ef7b7d7db384f1522829b7dc17bc
destors <halen54001@gmail.com>
2022-10-28 10:47:55 +0300

    destors: start update architecture on price-page

---
+++ libs/landing/price/page/src/lib/landing-price-page.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'olympia-kosiv-nx-landing-price-page',
+  templateUrl: './landing-price-page.component.html',
+  styleUrls: ['./landing-price-page.component.scss'],
+})
+export class LandingPricePageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ libs/landing/price/page/src/lib/landing-price-page.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LandingPricePageComponent } from './landing-price-page.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [LandingPricePageComponent],
+})
+export class LandingPricePageModule {}
