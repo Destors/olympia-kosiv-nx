393996b74a216384adadf5e568eaab426649066e
destors <halen54001@gmail.com>
2022-11-11 12:33:17 +0200

    destors: dell old price-pages

--- libs/core/navigation/common/src/lib/navigation.interface.ts
+++ libs/core/navigation/common/src/lib/navigation.interface.ts
@@ -3,11 +3,13 @@ import { InjectionToken } from '@angular/core';
 export interface NavigationPaths {
   home: string;
   price: string;
+  contacts: string;
 }
 
 export const NAVIGATION_PATHS: NavigationPaths = {
   home: '',
   price: 'price',
+  contacts: 'contacts',
 };
 
 export interface NavigationLink {
