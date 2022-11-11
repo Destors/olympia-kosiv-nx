73481f8576aa8ad0d577d24954aa93b343712cd0
destors <halen54001@gmail.com>
2022-11-11 18:00:31 +0200

    destors: Add basic to contact page

--- libs/landing/contact/page/src/lib/landing-contact-page.module.ts
+++ libs/landing/contact/page/src/lib/landing-contact-page.module.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { UiContainerModule } from '@olympia-kosiv-nx/ui/container';
 
 import { LandingContactPageComponent } from './landing-contact-page.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    UiContainerModule,
     RouterModule.forChild([
       { path: '', pathMatch: 'full', component: LandingContactPageComponent },
     ]),
