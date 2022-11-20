a4c07f2e61d6bdc2dfee25f9e1125fb8b46e9dd8
destors <halen54001@gmail.com>
2022-11-20 15:32:11 +0200

    destors: adv section with cuctom model start

--- libs/landing/home/ui/advantages/src/lib/landing-home-ui-advantages.component.ts
+++ libs/landing/home/ui/advantages/src/lib/landing-home-ui-advantages.component.ts
@@ -1,12 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
+export interface AdvCardModel {
+  icon: string;
+  title: string;
+  desc: string;
+}
 @Component({
   selector: 'olympia-kosiv-nx-landing-home-ui-advantages',
   templateUrl: './landing-home-ui-advantages.component.html',
   styleUrls: ['./landing-home-ui-advantages.component.scss'],
 })
-export class LandingHomeUiAdvantagesComponent implements OnInit {
-  constructor() {}
+export class LandingHomeUiAdvantagesComponent {
+  advCards = new Array<AdvCardModel>();
 
-  ngOnInit(): void {}
+  constructor() {
+    this.advCards = [
+      {
+        icon: 'svg',
+        title: 'Тренажерний зал',
+        desc: 'Займайтеся бодибілденгом, фітнесом або паверлфітенгом сучасному залі з великим вибором тренажерів.',
+      },
+      {
+        icon: 'svg',
+        title: 'Боротьба',
+        desc: 'Окрема секція для заняття боксом, мма та іншими видами боротьби',
+      },
+    ];
+  }
 }
