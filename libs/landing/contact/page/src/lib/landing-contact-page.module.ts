eb1bf00c2ea6068959aa6e2717ff5c78c830c75f
destors <halen54001@gmail.com>
2022-11-21 13:02:45 +0200

    destors: contacts to hero section

--- libs/landing/contact/page/src/lib/landing-contact-page.module.ts
+++ libs/landing/contact/page/src/lib/landing-contact-page.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { LandingHomeUiHeroImgModule } from '@olympia-kosiv-nx/landing/home/ui/hero-img';
 import { UiContainerModule } from '@olympia-kosiv-nx/ui/container';
 
 import { LandingContactPageComponent } from './landing-contact-page.component';
@@ -10,6 +11,7 @@ import { LandingContactPageComponent } from './landing-contact-page.component';
     CommonModule,
     UiContainerModule,
     NgOptimizedImage,
+    LandingHomeUiHeroImgModule,
     RouterModule.forChild([
       { path: '', pathMatch: 'full', component: LandingContactPageComponent },
     ]),
