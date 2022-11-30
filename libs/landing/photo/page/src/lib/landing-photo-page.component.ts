f227644628bdd6bd3a4a5c33e006450917821fd5
destors <halen54001@gmail.com>
2022-11-30 17:33:35 +0200

    destors: landing photo pege
    switch ui to page

--- libs/landing/photo/page/src/lib/landing-photo-page.component.ts
+++ libs/landing/photo/page/src/lib/landing-photo-page.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  DRESSING_ROOM_PHOTOS,
+  FIGHT_ROOM_PHOTOS,
+  MAIN_ROOM_PHOTOS,
+  OTHER_PHOTOS,
+  POWER_ROOM_PHOTOS,
+} from '@olympia-kosiv-nx/landing/photo/common';
 
 @Component({
   selector: 'olympia-kosiv-nx-landing-photo-page',
@@ -6,6 +13,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing-photo-page.component.scss'],
 })
 export class LandingPhotoPageComponent implements OnInit {
+  mainRoomPhotosArr = MAIN_ROOM_PHOTOS;
+  dressingRoomArr = DRESSING_ROOM_PHOTOS;
+  powerRoomArr = POWER_ROOM_PHOTOS;
+  fightRoomArr = FIGHT_ROOM_PHOTOS;
+  otherPhotoArr = OTHER_PHOTOS;
+
   constructor() {}
 
   ngOnInit(): void {}
