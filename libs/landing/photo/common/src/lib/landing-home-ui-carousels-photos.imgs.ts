3311dcff49d2ea87a3d824541c606d8c23cf4933
destors <halen54001@gmail.com>
2022-12-01 15:51:58 +0200

    destors: landing fix photo page

--- libs/landing/photo/common/src/lib/landing-home-ui-carousels-photos.imgs.ts
+++ libs/landing/photo/common/src/lib/landing-home-ui-carousels-photos.imgs.ts
@@ -1,36 +1,36 @@
 export const MAIN_ROOM_PHOTOS: string[] = [
-  '../assets/landing/carouselsPhotos/mainRoom/1.jpg',
-  '../assets/landing/carouselsPhotos/mainRoom/2.jpg',
-  '../assets/landing/carouselsPhotos/mainRoom/3.jpg',
-  '../assets/landing/carouselsPhotos/mainRoom/4.jpg',
-  '../assets/landing/carouselsPhotos/mainRoom/5.jpg',
-  '../assets/landing/carouselsPhotos/mainRoom/6.jpg',
+  '../assets/landing/carouselsPhotos/mainRoom/1.JPG',
+  '../assets/landing/carouselsPhotos/mainRoom/2.JPG',
+  '../assets/landing/carouselsPhotos/mainRoom/3.JPG',
+  '../assets/landing/carouselsPhotos/mainRoom/4.JPG',
+  '../assets/landing/carouselsPhotos/mainRoom/5.JPG',
+  '../assets/landing/carouselsPhotos/mainRoom/6.JPG',
 ];
 
 export const DRESSING_ROOM_PHOTOS: string[] = [
-  '../assets/landing/carouselsPhotos/dressingRoom/1.jpg',
-  '../assets/landing/carouselsPhotos/dressingRoom/2.jpg',
-  '../assets/landing/carouselsPhotos/dressingRoom/3.jpg',
-  '../assets/landing/carouselsPhotos/dressingRoom/4.jpg',
-  '../assets/landing/carouselsPhotos/dressingRoom/5.jpg',
+  '../assets/landing/carouselsPhotos/dressingRoom/1.JPG',
+  '../assets/landing/carouselsPhotos/dressingRoom/2.JPG',
+  '../assets/landing/carouselsPhotos/dressingRoom/3.JPG',
+  '../assets/landing/carouselsPhotos/dressingRoom/4.JPG',
+  '../assets/landing/carouselsPhotos/dressingRoom/5.JPG',
 ];
 
 export const POWER_ROOM_PHOTOS: string[] = [
-  '../assets/landing/carouselsPhotos/powerRoom/1.jpg',
-  '../assets/landing/carouselsPhotos/powerRoom/2.jpg',
-  '../assets/landing/carouselsPhotos/powerRoom/3.jpg',
-  '../assets/landing/carouselsPhotos/powerRoom/4.jpg',
+  '../assets/landing/carouselsPhotos/powerRoom/1.JPG',
+  '../assets/landing/carouselsPhotos/powerRoom/2.JPG',
+  '../assets/landing/carouselsPhotos/powerRoom/3.JPG',
+  '../assets/landing/carouselsPhotos/powerRoom/4.JPG',
 ];
 
 export const FIGHT_ROOM_PHOTOS: string[] = [
-  '../assets/landing/carouselsPhotos/fightRoom/1.jpg',
-  '../assets/landing/carouselsPhotos/fightRoom/2.jpg',
+  '../assets/landing/carouselsPhotos/fightRoom/1.JPG',
+  '../assets/landing/carouselsPhotos/fightRoom/2.JPG',
 ];
 
 export const OTHER_PHOTOS: string[] = [
-  '../assets/landing/carouselsPhotos/otherPhoto/1.jpg',
-  '../assets/landing/carouselsPhotos/otherPhoto/2.jpg',
-  '../assets/landing/carouselsPhotos/otherPhoto/3.jpg',
-  '../assets/landing/carouselsPhotos/otherPhoto/4.jpg',
-  '../assets/landing/carouselsPhotos/otherPhoto/5.jpg',
+  '../assets/landing/carouselsPhotos/otherPhoto/1.JPG',
+  '../assets/landing/carouselsPhotos/otherPhoto/2.JPG',
+  '../assets/landing/carouselsPhotos/otherPhoto/3.JPG',
+  '../assets/landing/carouselsPhotos/otherPhoto/4.JPG',
+  '../assets/landing/carouselsPhotos/otherPhoto/5.JPG',
 ];
--- libs/ui/carousel/src/lib/carousel-slide/carousel-slide.module.ts
+++ libs/ui/carousel/src/lib/carousel-slide/carousel-slide.module.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
-import { CarouselSlideComponent } from './carousel-slide.component';
 import { ImageModule } from '../../../../image/src/lib/image.module';
+import { CarouselSlideComponent } from './carousel-slide.component';
 
 @NgModule({
   imports: [CommonModule, ImageModule],
