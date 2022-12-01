8a75f5fb8c2891e0d1adc2c26591a0a2c3373ed7
destors <halen54001@gmail.com>
2022-12-01 17:52:21 +0200

    destors: price page add sort

--- libs/landing/price/page/src/lib/landing-price-page.component.ts
+++ libs/landing/price/page/src/lib/landing-price-page.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { LandingPricePageApiService } from '@olympia/landing/price/api';
 import { Prices } from '@olympia/landing/price/common';
-import { filter, map, Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 
 @Component({
   selector: 'olympia-landing-price-page',
@@ -18,21 +18,19 @@ export class LandingPricePageComponent {
   constructor(private pricePageService: LandingPricePageApiService) {
     this.prices$ = this.pricePageService.getAllPrices();
 
-    this.pricesWithCoach$ = this.getPricesWithCoach(true);
-    this.pricesWithoutCoach$ = this.getPricesWithCoach(false);
+    this.pricesWithCoach$ = this.filterPricesWithCoach(true);
+    this.pricesWithoutCoach$ = this.filterPricesWithCoach(false);
   }
 
-  getPricesWithCoach(coach: boolean): Observable<Prices[] | undefined> {
+  filterPricesWithCoach(coach: boolean): Observable<Prices[] | undefined> {
     return this.prices$.pipe(
-      map((val) => val?.filter((x) => (coach ? x.withCoach : !x.withCoach)))
+      map((val) =>
+        val
+          ?.sort((a, b) => {
+            return a.price < b.price ? -1 : 1;
+          })
+          ?.filter((x) => (coach ? x.withCoach : !x.withCoach))
+      )
     );
   }
-
-  // Creating mapped prices
-
-  // createMappedPrices(): Observable<Prices[]> {
-  //   return (this.mapedPrices$ = this.prices$.pipe(
-  //     map((val) => val.map((x) => ({ ...x, price: `${x.price} грн` })))
-  //   ));
-  // }
 }
