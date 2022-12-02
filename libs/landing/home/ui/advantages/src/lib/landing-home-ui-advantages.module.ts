9e3ff260e8f061cd6194553897de568b9bce1880
destors <halen54001@gmail.com>
2022-12-02 22:32:23 +0200

    destors: landing refactor advantages-card

--- libs/landing/home/ui/advantages/src/lib/landing-home-ui-advantages.module.ts
+++ libs/landing/home/ui/advantages/src/lib/landing-home-ui-advantages.module.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { UiGridModule } from '@olympia/ui/grid';
 
-import { LandingHomeUiAdvantagesCardModule } from './components/landing-home-ui-advantages-card/landing-home-ui-advantages-card.module';
+import { LandingHomeUiAdvantagesCardModule } from './components/card/landing-home-ui-advantages-card.module';
 import { LandingHomeUiAdvantagesComponent } from './landing-home-ui-advantages.component';
 
 @NgModule({
