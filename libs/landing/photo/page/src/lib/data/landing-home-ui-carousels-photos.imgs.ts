3b64562198cb0b83fc19b5adfeba10d9178996fa
destors <halen54001@gmail.com>
2022-12-05 19:08:12 +0200

    destors: landing photo page date, fix name

--- libs/landing/photo/page/src/lib/data/landing-home-ui-carousels-photos.imgs.ts
+++ libs/landing/photo/page/src/lib/data/landing-home-ui-carousels-photos.imgs.ts
@@ -1,6 +1,6 @@
 import { PhotosArrModel } from '@olympia/landing/photo/common';
 
-const MAIN_ROOM_PHOTOS: string[] = [
+const mainRoomPhotos: string[] = [
   '../assets/landing/carouselsPhotos/mainRoom/1.JPG',
   '../assets/landing/carouselsPhotos/mainRoom/2.JPG',
   '../assets/landing/carouselsPhotos/mainRoom/3.JPG',
@@ -9,7 +9,7 @@ const MAIN_ROOM_PHOTOS: string[] = [
   '../assets/landing/carouselsPhotos/mainRoom/6.JPG',
 ];
 
-const DRESSING_ROOM_PHOTOS: string[] = [
+const dressingRoomPhotos: string[] = [
   '../assets/landing/carouselsPhotos/dressingRoom/1.JPG',
   '../assets/landing/carouselsPhotos/dressingRoom/2.JPG',
   '../assets/landing/carouselsPhotos/dressingRoom/3.JPG',
@@ -17,23 +17,23 @@ const DRESSING_ROOM_PHOTOS: string[] = [
   '../assets/landing/carouselsPhotos/dressingRoom/5.JPG',
 ];
 
-const POWER_ROOM_PHOTOS: string[] = [
+const powerRoomPhotos: string[] = [
   '../assets/landing/carouselsPhotos/powerRoom/1.JPG',
   '../assets/landing/carouselsPhotos/powerRoom/2.JPG',
   '../assets/landing/carouselsPhotos/powerRoom/3.JPG',
   '../assets/landing/carouselsPhotos/powerRoom/4.JPG',
 ];
 
-const FIGHT_ROOM_PHOTOS: string[] = [
+const fightRoomPhotos: string[] = [
   '../assets/landing/carouselsPhotos/fightRoom/1.JPG',
   '../assets/landing/carouselsPhotos/fightRoom/2.JPG',
 ];
 
 export function getCarouselPhotos(): PhotosArrModel[] {
   return [
-    { title: 'Головна зала', imgArr: [...MAIN_ROOM_PHOTOS] },
-    { title: 'Роздягальні та душ', imgArr: [...DRESSING_ROOM_PHOTOS] },
-    { title: 'Силова зала', imgArr: [...POWER_ROOM_PHOTOS] },
-    { title: 'Татамі та бородьба', imgArr: [...FIGHT_ROOM_PHOTOS] },
+    { title: 'Головна зала', imgArr: [...mainRoomPhotos] },
+    { title: 'Роздягальні та душ', imgArr: [...dressingRoomPhotos] },
+    { title: 'Силова зала', imgArr: [...powerRoomPhotos] },
+    { title: 'Татамі та бородьба', imgArr: [...fightRoomPhotos] },
   ];
 }
