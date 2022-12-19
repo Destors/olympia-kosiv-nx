d11edc1d8afa7d026c82cc271598b5edc0c2f0e9
destors <halen54001@gmail.com>
2022-12-19 19:06:01 +0200

    destors: update meta interface

--- libs/core/meta/common/src/lib/meta.interface.ts
+++ libs/core/meta/common/src/lib/meta.interface.ts
@@ -23,17 +23,18 @@ export const META_CONFIG = new InjectionToken<MetaConfig>('MetaConfig');
 export const META_CONFIG_OG = new InjectionToken<MetaConfigOg>('MetaConfigOg');
 
 export const META_CONFIG_DEFAULT: MetaConfig = {
-  title: `:Meta default|:Online store Banshop`,
+  title: 'Тренажерний зал Olympia-Kosiv',
   // eslint-disable-next-line max-len
-  description: `:Meta default|:Banshop sportswear, shoes and accessories. Free delivery in Moscow and all over Russia when paying on the website.`,
-  keywords: `:Meta default|:sneakers, sports shoes`,
+  description:
+    'Пропонуємо комфортне тренування у спортивному залі оснащеному якісним обладнанням та приємними цінами у місті Косів',
+  keywords: 'Тренажерний зал, душ, спорт зал, GYM, фітнес клуб косів',
 };
 
 export const META_CONFIG_OG_DEFAULT: MetaConfigOg = {
   title: META_CONFIG_DEFAULT.title,
   description: META_CONFIG_DEFAULT.description,
   type: 'website',
-  image: '/assets/images/site.jpg',
+  image: '/assets/meta/meta.jpg',
   imageType: 'image/jpeg',
   imageWidth: '1200',
   imageHeight: '600',
